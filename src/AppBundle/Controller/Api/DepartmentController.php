4ff5ab033828487a289da3827e2c374ca6358710
monzey <maxi.bertrand@gmail.com>
2015-11-01 20:06:17 +0100

    departments: added PUT action

--- src/AppBundle/Controller/Api/DepartmentController.php
+++ src/AppBundle/Controller/Api/DepartmentController.php
@@ -3,11 +3,14 @@
 namespace AppBundle\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\Department;
+use AppBundle\Form\DepartmentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class DepartmentController extends FOSRestController
@@ -37,6 +40,49 @@ class DepartmentController extends FOSRestController
     public function getDepartmentAction(Department $department)
     {
         return $department;
+    }
+
+    /**
+     * Modifie un départment
+     * PUT api/departments/{slug}
+     * @Rest\View()
+     * @ParamConverter("department", class="AppBundle:Department")
+     *
+     * @param  Request    $request    [description]
+     * @param  Department $department [description]
+     * @return [type]                 [description]
+     */
+    public function putDepartmentAction(Request $request, Department $department)
+    {
+
+        $editForm = $this->createEditForm($department);
+
+        $editForm->submit($request->get($editForm->getName()));
+
+        if ($editForm->isValid() && $editForm->isSubmitted()) {
+
+            $this->getDoctrine()->getManager()->persist($department);
+            $this->getDoctrine()->getManager()->flush();
+
+            return new JsonResponse($this->get('serializer')->toArray($department), 200);
+
+        }
+
+        return new JsonResponse($this->get('serializer')->toArray($editForm->getErrors()));
+
+    }
+
+    /**
+     * Retourne le formulaire d'édition d'un départment
+     *
+     * @param  [type] $department [description]
+     * @return [type]             [description]
+     */
+    protected function createEditForm($department)
+    {
+        $editForm = $this->createForm(new DepartmentType(), $department);
+
+        return $editForm;
     }
 
 }
--- src/AppBundle/Entity/Department.php
+++ src/AppBundle/Entity/Department.php
@@ -104,4 +104,69 @@ class Department
     {
         return $this->number;
     }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set county
+     *
+     * @param \AppBundle\Entity\County $county
+     *
+     * @return Department
+     */
+    public function setCounty(\AppBundle\Entity\County $county = null)
+    {
+        $this->county = $county;
+
+        return $this;
+    }
+
+    /**
+     * Get county
+     *
+     * @return \AppBundle\Entity\County
+     */
+    public function getCounty()
+    {
+        return $this->county;
+    }
+
+    /**
+     * Add user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Department
+     */
+    public function addUser(\AppBundle\Entity\User $user)
+    {
+        $this->users[] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param \AppBundle\Entity\User $user
+     */
+    public function removeUser(\AppBundle\Entity\User $user)
+    {
+        $this->users->removeElement($user);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
 }
