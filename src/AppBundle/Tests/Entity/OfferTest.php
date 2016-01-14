e5cc1f4a2df6fa306c2d2e87592c963a94cb7966
monzey <maxi.bertrand@gmail.com>
2016-01-14 19:51:36 +0100

    added tests for entity Offer

---
+++ src/AppBundle/Tests/Entity/OfferTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace AppBundle\Tests\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class OfferTest extends WebTestCase
+{
+
+    public function tearDown()
+    {
+        return;
+    }
+
+    public function testDummy()
+    {
+        return $this->assertNull(null);
+    }
+
+}
