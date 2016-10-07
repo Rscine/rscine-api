a862aa3170fee5b7d646b08faacd66d755c2309f
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-10-07 15:33:44 +0200

    Revert "Suppression de dossiers inutiles pour ne garder que les classes du corps"
    This reverts commit ad8851ab73824478d9ea843e082fdea91f399dae.

---
+++ app/AppKernel.php
@@ -0,0 +1,50 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            // new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Rscine\UserBundle\RscineUserBundle(),
+            new Rscine\OfferBundle\RscineOfferBundle(),
+            new Rscine\CoreBundle\RscineCoreBundle(),
+            new Rscine\WorkerBundle\RscineWorkerBundle(),
+            new Rscine\OAuthServerBundle\RscineOAuthServerBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
--- src/Rscine/ModelBundle/RscineModelBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rscine\ModelBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RscineModelBundle extends Bundle
-{
-}
