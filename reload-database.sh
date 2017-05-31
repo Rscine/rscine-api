6ccc52cbaa1a2cb50bb1fcefed90c4e642a1292a
Maxime Bertrand <maxi.bertrand@gmail.com>
2017-05-31 15:42:46 +0200

    fixed reload-database command shortcut (app/console => bin/console)

--- reload-database.sh
+++ reload-database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php app/console doctrine:database:drop --force
-php app/console doctrine:database:create
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load
